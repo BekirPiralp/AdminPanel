c09cea8b134b23b3d7f4f7ca8f870f2b5a80d07c
Bekir PİRALP <bpir33@gmail.com>
2022-05-27 09:33:53 +0300

    Veritabanı nesnelerinin şuan ki hali ayarlandı lakin;
        Firama nesnesinde geliştirme yapılacak
    
        Bayiler nesnesi olusturulacak
    
        Her bayide kayıtlı müşteri olması için müşteri nesnesi oluşturulacak
    
        Firma ve Bayi için yönetici nesneleri oluşturulacak ve  bunlara uygun şifreler kısmı

---
+++ AdminPanel.EntityLayer/Concrete/Order/HizmetAlt.cs
@@ -0,0 +1,10 @@
+﻿using AdminPanel.EntityLayer.Concrete.Base;
+
+namespace AdminPanel.EntityLayer.Concrete.Order
+{
+    public class HizmetAlt : EntityBase
+    {
+        public int hizmet { get; set; }
+        public string isim { get; set; }
+    }
+}
---
+++ AdminPanel.EntityLayer/Concrete/Order/HizmetAltOzellik.cs
@@ -0,0 +1,10 @@
+﻿using AdminPanel.EntityLayer.Concrete.Base;
+
+namespace AdminPanel.EntityLayer.Concrete.Order
+{
+    public class HizmetAltOzellik : EntityBase
+    {
+        public int hizmetAlt { get; set; }
+        public string isim { get; set; }
+    }
+}
