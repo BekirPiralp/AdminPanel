474aa9fe0268b7fe322fface0b5573a72fa63966
Bekir PİRALP <bpir33@gmail.com>
2022-05-31 10:12:48 +0300

    İşkatmanı oluşturulmaya başlandı

---
+++ AdminPanle.BussinesLayer/Abstract/Base/IEntityBusBase.cs
@@ -0,0 +1,29 @@
+﻿using AdminPanel.EntityLayer.Abctract;
+
+namespace AdminPanle.BusinessLayer.Abstract.Base
+{
+    // iş katmanı genel arayüzü
+    public interface IEntityBusBase<TEntity>
+        where TEntity : IEntity, new ()
+    {
+        #region Getirme işlemleri
+        /**
+         * Sil durumuna bakmaksızın geliyor
+         */ 
+
+        /// <summary>
+        /// Liste olarak getirme 
+        /// </summary>
+        /// <returns></returns>
+        Task<List<TEntity>> GetAllAsync();
+        
+        /// <summary>
+        /// id ye göre
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<TEntity> GetByIdAsync(int id);
+        
+        #endregion
+    }
+}
