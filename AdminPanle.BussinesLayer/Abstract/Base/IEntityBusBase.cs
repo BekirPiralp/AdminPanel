b3bf456b04a301cfa5cf5f02b68f41588efa8eb5
Bekir PİRALP <bpir33@gmail.com>
2022-05-31 10:48:44 +0300

    Bussines layer için generic form interface'i oluşturuldu. Temel methodlar belirlendi

--- AdminPanle.BussinesLayer/Abstract/Base/IEntityBusBase.cs
+++ AdminPanle.BussinesLayer/Abstract/Base/IEntityBusBase.cs
@@ -7,10 +7,7 @@ namespace AdminPanle.BusinessLayer.Abstract.Base
         where TEntity : IEntity, new ()
     {
         #region Getirme işlemleri
-        /**
-         * Sil durumuna bakmaksızın geliyor
-         */ 
-
+        
         /// <summary>
         /// Liste olarak getirme 
         /// </summary>
@@ -23,7 +20,77 @@ namespace AdminPanle.BusinessLayer.Abstract.Base
         /// <param name="id"></param>
         /// <returns></returns>
         Task<TEntity> GetByIdAsync(int id);
-        
+        #endregion
+
+        #region Silme işlemleri
+
+        /// <summary>
+        /// Liste olarak siler
+        /// sonuç başarılı ise true döndürür
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        Task<bool> Delete(List<TEntity> entities);
+
+        /// <summary>
+        /// id olarak siler
+        /// sonuç başarılı ise true döndürür
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<bool> Delete(int id);
+
+        /// <summary>
+        /// Göderilmiş olan nesneye göre siler
+        /// sonuç başarılı ise true döndürür
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<bool> Delete(TEntity entity);
+        #endregion
+
+        #region Güncelleme işlemleri
+
+        /// <summary>
+        /// Güncelleme işlemi yapar
+        /// işlem başarılı ise true diğer durumlarda false
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<bool> Update(TEntity entity);
+
+        /// <summary>
+        /// Güncelleme işlemi yapar
+        /// işlem başarılı ise nesneyi döndürür
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<TEntity> UpdateBy(TEntity entity);
+        #endregion
+
+        #region Ekleme işlemleri
+        /// <summary>
+        /// Ekleme işlemi yapar teknesne için
+        /// sonuç başarılı ise true döndürür
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<bool> Add(TEntity entity);
+
+        /// <summary>
+        /// Liste olarak ekler
+        /// sonuç başarılı ise true döndürür
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<bool> Add(List<TEntity> entity);
+
+        /// <summary>
+        /// Ekleme işlemi yapar işlem başarılı ise kayıtlı halini geri dödürür
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<TEntity> AddBy(TEntity entity);
         #endregion
     }
 }
