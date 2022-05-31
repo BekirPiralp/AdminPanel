cd452432e50ca17f3ca35561c55c87c19ac475da
Bekir PİRALP <bpir33@gmail.com>
2022-05-31 16:05:10 +0300

    Paket kısmı yazıldı

---
+++ AdminPanle.BussinesLayer/Abstract/Other/PaketKismi/IBusPaket.cs
@@ -0,0 +1,9 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.PaketKismi;
+using AdminPanle.BusinessLayer.Abstract.Base;
+
+namespace AdminPanle.BusinessLayer.Abstract.Other.PaketKismi
+{
+    public interface IBusPaket : IEntityBusBase<Paket>
+    {
+    }
+}
