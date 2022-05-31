78c8d16b6aa22290b0dc671036241cdf29ecd39e
Bekir PİRALP <bpir33@gmail.com>
2022-05-31 15:22:57 +0300

    İş yapma katmani diğerleri Hizmet kısmı bitti.

---
+++ AdminPanle.BussinesLayer/Abstract/Other/HizmetKismi/IBusHizmetAlt.cs
@@ -0,0 +1,9 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.HizmetKismi;
+using AdminPanle.BusinessLayer.Abstract.Base;
+
+namespace AdminPanle.BusinessLayer.Abstract.Other.HizmetKismi
+{
+    public interface IBusHizmetAlt : IEntityBusBase<HizmetAlt>
+    {
+    }
+}
