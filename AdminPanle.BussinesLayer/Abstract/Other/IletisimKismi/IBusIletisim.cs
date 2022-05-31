9233bfce2ad7678271751edd993a77c01c8d86a7
Bekir PİRALP <bpir33@gmail.com>
2022-05-31 15:30:33 +0300

    iletisim ve ipucu-yazi kisimlari yazildi

---
+++ AdminPanle.BussinesLayer/Abstract/Other/IletisimKismi/IBusIletisim.cs
@@ -0,0 +1,9 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.IletisimKismi;
+using AdminPanle.BusinessLayer.Abstract.Base;
+
+namespace AdminPanle.BusinessLayer.Abstract.Other.IletisimKismi
+{
+    public interface IBusIletisim : IEntityBusBase<Iletisim>
+    {
+    }
+}
---
+++ AdminPanle.BussinesLayer/Abstract/Other/IpucuVeYaziKismi/IBusIpucuVeYazi.cs
@@ -0,0 +1,9 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.IpucuKismi;
+using AdminPanle.BusinessLayer.Abstract.Base;
+
+namespace AdminPanle.BusinessLayer.Abstract.Other.IpucuVeYaziKismi
+{
+    public interface IBusIpucuVeYazi : IEntityBusBase<IpucuVeYazi>
+    {
+    }
+}
