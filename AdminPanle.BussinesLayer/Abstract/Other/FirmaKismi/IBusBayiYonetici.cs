db3fd5a39e31e2e629416b3feecadc13350c992e
Bekir PİRALP <bpir33@gmail.com>
2022-05-31 15:05:29 +0300

    Business Layer Other Firma Kısmı yazıldı.

---
+++ AdminPanle.BussinesLayer/Abstract/Other/FirmaKismi/IBusBayiYonetici.cs
@@ -0,0 +1,9 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.FirmaKismi;
+using AdminPanle.BusinessLayer.Abstract.Base;
+
+namespace AdminPanle.BusinessLayer.Abstract.Other.FirmaKismi
+{
+    public interface IBusBayiYonetici : IEntityBusBase<BayiYonetici>
+    {
+    }
+}
---
+++ AdminPanle.BussinesLayer/Abstract/Other/FirmaKismi/IBusFirma.cs
@@ -0,0 +1,9 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.FirmaKismi;
+using AdminPanle.BusinessLayer.Abstract.Base;
+
+namespace AdminPanle.BusinessLayer.Abstract.Other.FirmaKismi
+{
+    public interface IBusFirma : IEntityBusBase<Firma>
+    {
+    }
+}
