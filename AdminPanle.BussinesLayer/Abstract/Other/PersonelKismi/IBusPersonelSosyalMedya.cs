7d82bf468cb42762a2b3c9ebbebaa583d3fc826d
Bekir PİRALP <bpir33@gmail.com>
2022-05-31 16:10:56 +0300

    Peronsel kısmı tamamlandı

---
+++ AdminPanle.BussinesLayer/Abstract/Other/PersonelKismi/IBusPersonelSosyalMedya.cs
@@ -0,0 +1,9 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.PersonelKismi;
+using AdminPanle.BusinessLayer.Abstract.Base;
+
+namespace AdminPanle.BusinessLayer.Abstract.Other.PersonelKismi
+{
+    public interface IBusPersonelSosyalMedya : IEntityBusBase<PersonelSosyalMedya>
+    {
+    }
+}
