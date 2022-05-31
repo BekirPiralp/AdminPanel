af7f93eee9fe61f4606c5ed76fed1ba767970848
Bekir PİRALP <bpir33@gmail.com>
2022-05-31 16:13:44 +0300

    iş yapma katmanı soyut kısımda son olan refereans kısmı yazıldı. somut kısma geçiliyor...

---
+++ AdminPanle.BussinesLayer/Abstract/Other/ReferansKismi/IBusReferans.cs
@@ -0,0 +1,9 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.ReferansKismi;
+using AdminPanle.BusinessLayer.Abstract.Base;
+
+namespace AdminPanle.BusinessLayer.Abstract.Other.ReferansKismi
+{
+    public interface IBusReferans : IEntityBusBase<Referans>
+    {
+    }
+}
