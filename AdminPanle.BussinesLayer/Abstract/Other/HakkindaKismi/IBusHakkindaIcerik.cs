7e49f4bc0f42d6fdfd71bad871ddd73b8db1aa5c
Bekir PİRALP <bpir33@gmail.com>
2022-05-31 15:12:22 +0300

    İş Katmanı soyut diğerleri Hakkinda kısmı tamalandı.

---
+++ AdminPanle.BussinesLayer/Abstract/Other/HakkindaKismi/IBusHakkindaIcerik.cs
@@ -0,0 +1,9 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.HakkindaKismi;
+using AdminPanle.BusinessLayer.Abstract.Base;
+
+namespace AdminPanle.BusinessLayer.Abstract.Other.HakkindaKismi
+{
+    public interface IBusHakkindaIcerik : IEntityBusBase<HakkindaIcerik>
+    {
+    }
+}
