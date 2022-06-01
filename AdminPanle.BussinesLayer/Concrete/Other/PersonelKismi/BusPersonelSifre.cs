cfb420f94e286ce6956e1c5c34d2637cf11ca913
Bekir PİRALP <bpir33@gmail.com>
2022-06-01 14:18:57 +0300

    Personel Kısmı yazıldı.

---
+++ AdminPanle.BussinesLayer/Concrete/Other/PersonelKismi/BusPersonelSifre.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.DataAccessLayer.Abstract.Other.Genel.PersonelKismi;
+using AdminPanel.EntityLayer.Concrete.Other.PersonelKismi;
+using AdminPanle.BusinessLayer.Abstract.Other.PersonelKismi;
+using AdminPanle.BusinessLayer.Concrete.Base;
+
+namespace AdminPanle.BusinessLayer.Concrete.Other.PersonelKismi
+{
+    public class BusPersonelSifre : EntityBusBase<PersonelSifre>, IBusPersonelSifre
+    {
+        public BusPersonelSifre(IDalPersonelSifre entityDalBase) : base(entityDalBase)
+        {
+        }
+    }
+}
---
+++ AdminPanle.BussinesLayer/Concrete/Other/PersonelKismi/BusPersonelSosyalMedya.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.DataAccessLayer.Abstract.Other.Genel.PersonelKismi;
+using AdminPanel.EntityLayer.Concrete.Other.PersonelKismi;
+using AdminPanle.BusinessLayer.Abstract.Other.PersonelKismi;
+using AdminPanle.BusinessLayer.Concrete.Base;
+
+namespace AdminPanle.BusinessLayer.Concrete.Other.PersonelKismi
+{
+    public class BusPersonelSosyalMedya : EntityBusBase<PersonelSosyalMedya>, IBusPersonelSosyalMedya
+    {
+        public BusPersonelSosyalMedya(IDalPersonelSosyalMedya entityDalBase) : base(entityDalBase)
+        {
+        }
+    }
+}
