644f2611aa3efa36076e2324a699ea852ee8039b
Bekir PİRALP <bpir33@gmail.com>
2022-06-01 14:28:10 +0300

    Referans kısmı yazılmasıyla birlikte İş yapma katmanı için şimdilik yazma işlemi bitti. Veri erişim katmanında ise nesne oluşturucu yazmaya başlanacaktır.

---
+++ AdminPanle.BussinesLayer/Concrete/Other/ReferansKismi/BusReferans.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.DataAccessLayer.Abstract.Other.Genel.ReferansKismi;
+using AdminPanel.EntityLayer.Concrete.Other.ReferansKismi;
+using AdminPanle.BusinessLayer.Abstract.Other.ReferansKismi;
+using AdminPanle.BusinessLayer.Concrete.Base;
+
+namespace AdminPanle.BusinessLayer.Concrete.Other.ReferansKismi
+{
+    public class BusReferans : EntityBusBase<Referans>, IBusReferans
+    {
+        public BusReferans(IDalReferans entityDalBase) : base(entityDalBase)
+        {
+        }
+    }
+}
