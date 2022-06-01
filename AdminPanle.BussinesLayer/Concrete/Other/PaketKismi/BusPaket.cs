3798e59e606f763550cde0ec7d9783ea449b672e
Bekir PİRALP <bpir33@gmail.com>
2022-06-01 13:55:39 +0300

    Mesaj - Musteri - Paket kısımları ayarlandı.

---
+++ AdminPanle.BussinesLayer/Concrete/Other/PaketKismi/BusPaket.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.DataAccessLayer.Abstract.Other.Genel.PaketKismi;
+using AdminPanel.EntityLayer.Concrete.Other.PaketKismi;
+using AdminPanle.BusinessLayer.Abstract.Other.PaketKismi;
+using AdminPanle.BusinessLayer.Concrete.Base;
+
+namespace AdminPanle.BusinessLayer.Concrete.Other.PaketKismi
+{
+    public class BusPaket : EntityBusBase<Paket>, IBusPaket
+    {
+        public BusPaket(IDalPaket entityDalBase) : base(entityDalBase)
+        {
+        }
+    }
+}
---
+++ AdminPanle.BussinesLayer/Concrete/Other/PaketKismi/BusPaketOzellik.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.DataAccessLayer.Abstract.Other.Genel.PaketKismi;
+using AdminPanel.EntityLayer.Concrete.Other.PaketKismi;
+using AdminPanle.BusinessLayer.Abstract.Other.PaketKismi;
+using AdminPanle.BusinessLayer.Concrete.Base;
+
+namespace AdminPanle.BusinessLayer.Concrete.Other.PaketKismi
+{
+    internal class BusPaketOzellik : EntityBusBase<PaketOzellik>, IBusPaketOzellik
+    {
+        public BusPaketOzellik(IDalPaketOzellik entityDalBase) : base(entityDalBase)
+        {
+        }
+    }
+}
