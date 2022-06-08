2bd2451816c55912953f9b1779f38b2ddec9c7fe
Bekir PİRALP <bpir33@gmail.com>
2022-06-08 12:08:56 +0300

    Paket Kısmı için temel kontroller oluşturuldu.

---
+++ AdminPanel.WebAPI/Controllers/Settled/PaketKismi/PaketController.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.PaketKismi;
+using AdminPanel.WebAPI.Controllers.Base;
+using AdminPanle.BusinessLayer.Abstract.Other.PaketKismi;
+using AdminPanle.BusinessLayer.Other;
+
+namespace AdminPanel.WebAPI.Controllers.Settled.PaketKismi
+{
+    public class PaketController : BaseController<IBusPaket, Paket>
+    {
+        public PaketController() : base(BusOlusturucu.Olustur().Paket)
+        {
+        }
+    }
+}
