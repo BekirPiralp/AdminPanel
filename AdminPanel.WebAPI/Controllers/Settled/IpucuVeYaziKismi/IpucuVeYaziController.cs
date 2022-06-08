b3f681f3f7bffda62cd484e268e24d9b3740f8cc
Bekir PİRALP <bpir33@gmail.com>
2022-06-08 12:04:13 +0300

    Ipupcu ve yazi - Logo - Mesaj -Musteri kısımları için temel kontroller eklendi.

---
+++ AdminPanel.WebAPI/Controllers/Settled/IpucuVeYaziKismi/IpucuVeYaziController.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.IpucuKismi;
+using AdminPanel.WebAPI.Controllers.Base;
+using AdminPanle.BusinessLayer.Abstract.Other.IpucuKismi;
+using AdminPanle.BusinessLayer.Other;
+
+namespace AdminPanel.WebAPI.Controllers.Settled.IpucuVeYaziKismi
+{
+    public class IpucuVeYaziController : BaseController<IBusIpucuVeYazi, IpucuVeYazi>
+    {
+        public IpucuVeYaziController() : base(BusOlusturucu.Olustur().IpucuVeYazi)
+        {
+        }
+    }
+}
---
+++ AdminPanel.WebAPI/Controllers/Settled/LogoKismi/SosyalMedyaLogoController.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.LogoKismi;
+using AdminPanel.WebAPI.Controllers.Base;
+using AdminPanle.BusinessLayer.Abstract.Other.LogoKismi;
+using AdminPanle.BusinessLayer.Other;
+
+namespace AdminPanel.WebAPI.Controllers.Settled.LogoKismi
+{
+    public class SosyalMedyaLogoController : BaseController<IBusSosyalMedyaLogo, SosyalMedyaLogo>
+    {
+        public SosyalMedyaLogoController() : base(BusOlusturucu.Olustur().SosyalMedyaLogo)
+        {
+        }
+    }
+}
