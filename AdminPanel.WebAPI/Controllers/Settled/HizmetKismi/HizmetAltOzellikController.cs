3ff6fc72a1a60ad49739a3e4d45dd2622a7d28c1
Bekir PİRALP <bpir33@gmail.com>
2022-06-08 09:19:55 +0300

    Hizmet Kısmı için controller ayarlandı

---
+++ AdminPanel.WebAPI/Controllers/Settled/HizmetKismi/HizmetAltOzellikController.cs
@@ -0,0 +1,14 @@
+﻿using AdminPanel.EntityLayer.Concrete.Other.HizmetKismi;
+using AdminPanel.WebAPI.Controllers.Base;
+using AdminPanle.BusinessLayer.Abstract.Other.HizmetKismi;
+using AdminPanle.BusinessLayer.Other;
+
+namespace AdminPanel.WebAPI.Controllers.Settled.HizmetKismi
+{
+    public class HizmetAltOzellikController : BaseController<IBusHizmetAltOzellik, HizmetAltOzellik>
+    {
+        public HizmetAltOzellikController() : base(BusOlusturucu.Olustur().HizmetAltOzellik)
+        {
+        }
+    }
+}
