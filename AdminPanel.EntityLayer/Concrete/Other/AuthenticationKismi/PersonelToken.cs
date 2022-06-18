d411033cbb5a0c01432d862d26b017151233a89f
Bekir PİRALP <bpir33@gmail.com>
2022-06-18 16:01:49 +0300

    PersonelToken classı yazıldı ve entity base sınıfından kalıtım aldı

---
+++ AdminPanel.EntityLayer/Concrete/Other/AuthenticationKismi/PersonelToken.cs
@@ -0,0 +1,10 @@
+﻿using AdminPanel.EntityLayer.Concrete.Base;
+
+namespace AdminPanel.EntityLayer.Concrete.Other.AuthenticationKismi
+{
+    public class PersonelToken : EntityBase
+    {
+        public string? RefreshToken {get;set;}
+        public DateTime? RefreshTokenDate {get;set;}
+    }
+}
