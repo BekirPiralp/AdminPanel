08f8efd277f23a5f4c5ae756984d5323754749d0
Bekir PİRALP <bpir33@gmail.com>
2022-06-22 16:08:14 +0300

    .

--- AdminPanel.WebAPI/Extension/SwagerExtensions.cs
+++ AdminPanel.WebAPI/Extension/SwagerExtensions.cs
@@ -7,48 +7,42 @@ namespace AdminPanel.WebAPI.Extension
     {
         internal static void AddSwagerAuthorization(this IServiceCollection services)
         {
-            services.AddSwaggerGen(
-                opt =>
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo()
                 {
-                    opt.SwaggerDoc("v1", new OpenApiInfo
-                    {
-                        Title = "Admin paneli API",
-                        Version = "v001",
-                        Description = "Admin panelinin api kısmıdır ve bir araba kiralam şirketine hizmet edecektir."
+                    Title = "Admin paneli API",
+                    Version = "v01",
+                    //Description = "Admin panelinin api kısmıdır ve bir araba kiralam şirketine hizmet edecektir."
+                });
+
+                opt.AddSecurityDefinition("Güvenlik Kısmı",
+                    new OpenApiSecurityScheme(){
+                        In = ParameterLocation.Header,
+                        Description = "Lütfen tokenı giriniz",
+                        Name = "Authorize",
+                        Type = SecuritySchemeType.Http,
+                        BearerFormat = "JWT",
+                        Scheme = "Bearer"//JwtBearerDefaults.AuthenticationScheme
                     });
 
-                    opt.AddSecurityDefinition("Güvenlik Kısmı",
-                        new OpenApiSecurityScheme
-                        {
-                            In = ParameterLocation.Header,
-                            Description = "Lütfen tokenı giriniz",
-                            Name = "Authorize",
-                            Type = SecuritySchemeType.Http,
-                            BearerFormat = "JWT",
-                            Scheme = JwtBearerDefaults.AuthenticationScheme
-                        }
-                        );
-
-                    var oASS = new OpenApiSecurityScheme
+                var oASS = new OpenApiSecurityScheme()
+                {
+                    Reference = new OpenApiReference
                     {
-                        Reference = new OpenApiReference
-                        {
-                            Id = JwtBearerDefaults.AuthenticationScheme,
-                            Type = ReferenceType.SecurityScheme
-                        }
-                    };
+                        Id = "Bearer",//JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
 
-                    var value = new List<string>();
+                var value = new List<string>();
 
-                    //Dictionary<OpenApiSecurityScheme,IList<string>> val =  
-                    //new Dictionary<OpenApiSecurityScheme, IList<string>>(){ {oASS,value } };
-                    //val.Add(oASS, value);
-
-                    opt.AddSecurityRequirement(new OpenApiSecurityRequirement{{
-                            oASS,value
-                        }}
-                    );
-                });
+                
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement(){{
+                        oASS,value
+                    }}
+                );
+            });
         }
     }
 }
