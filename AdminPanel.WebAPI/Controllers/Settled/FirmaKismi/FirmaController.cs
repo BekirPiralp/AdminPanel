f6af95be7049d55b9c78ee9be6c723d19a161a61
Bekir PİRALP <bpir33@gmail.com>
2022-06-28 21:39:09 +0300

    Yaanlışlıkla constracture dan IbusFirma nesnesi istemişim o düzeltildi.

--- AdminPanel.WebAPI/Controllers/Settled/FirmaKismi/FirmaController.cs
+++ AdminPanel.WebAPI/Controllers/Settled/FirmaKismi/FirmaController.cs
@@ -1,12 +1,13 @@
 ﻿using AdminPanel.EntityLayer.Concrete.Other.FirmaKismi;
 using AdminPanel.WebAPI.Controllers.Base;
 using AdminPanle.BusinessLayer.Abstract.Other.FirmaKismi;
+using AdminPanle.BusinessLayer.Other;
 
 namespace AdminPanel.WebAPI.Controllers.Settled.FirmaKismi
 {
     public class FirmaController : BaseController<IBusFirma, Firma>
     {
-        public FirmaController(IBusFirma entityBusBase) : base(entityBusBase)
+        public FirmaController() : base(BusOlusturucu.Olustur().Firma)
         {
         }
     }
