fde9e5a6dab0a946a803c936b8cb568a338465f5
Bekir PİRALP <bpir33@gmail.com>
2022-12-03 22:55:27 +0300

    postgre sql bağlantı bilgileri ayarlandı

--- AdminPanel.AppSettings/AppSettingsConfigurationNames.cs
+++ AdminPanel.AppSettings/AppSettingsConfigurationNames.cs
@@ -5,5 +5,6 @@
         public readonly static string OracleDbString = "OracleString";
         public readonly static string OracleSqlComponity = "Oracle";
         public readonly static string MsSqlDbString = "Varsayilan";
+        public readonly static string PostgreSQL = "testPostgreSQL";
     }
 }
