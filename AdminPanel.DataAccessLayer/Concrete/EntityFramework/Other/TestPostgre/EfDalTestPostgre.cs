f5627d3f9f177aacb1de804b36d1a3cef8f5e52a
Bekir PİRALP <bpir33@gmail.com>
2022-12-03 23:43:34 +0300

    Postgre için context ayarlandı ilgili paketler yüklendi. Context eklenmesi gereken yere context eklendi.

--- AdminPanel.DataAccessLayer/Concrete/EntityFramework/Other/TestPostgre/EfDalTestPostgre.cs
+++ AdminPanel.DataAccessLayer/Concrete/EntityFramework/Other/TestPostgre/EfDalTestPostgre.cs
@@ -1,11 +1,10 @@
 ﻿using AdminPanel.DataAccessLayer.Abstract.Other.Genel.TestPostgre;
 using AdminPanel.DataAccessLayer.Concrete.EntityFramework.Base.Postgre;
 using AdminPanel.EntityLayer.Concrete.Other.TestPostgre;
-using Microsoft.AspNetCore.Server.IIS.Core;
 
 namespace AdminPanel.DataAccessLayer.Concrete.EntityFramework.Other.TestPostgre
 {
-    public class EfDalTestPostgre : EntityEfDalPostgre<testpostgre, context gelecek>, ITestPostgreDal
+    public class EfDalTestPostgre : EntityEfDalPostgre<testpostgre,EfContextPostgre>, ITestPostgreDal
     {
     }
 }
