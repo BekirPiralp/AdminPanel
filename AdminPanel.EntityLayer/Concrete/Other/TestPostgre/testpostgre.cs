f91fbf73d35f15699d7a1468db89387b71a7996d
Bekir PİRALP <bpir33@gmail.com>
2022-12-03 23:06:11 +0300

    Entity oluşturuldu

---
+++ AdminPanel.EntityLayer/Concrete/Other/TestPostgre/testpostgre.cs
@@ -0,0 +1,10 @@
+﻿using AdminPanel.EntityLayer.Abctract;
+
+namespace AdminPanel.EntityLayer.Concrete.Other.TestPostgre
+{
+    public class testpostgre : IEntity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
