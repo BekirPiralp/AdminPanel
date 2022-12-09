36962f0b51d7cef9ae7c3befbcf5da4c62c03f90
alitas89 <alitas@outlook.com>
2022-12-09 11:42:38 +0300

    Pruduct Controller added unit tests

---
+++ AdminPanel.WebAPITests/Controllers/Settled/TestProduct/ProductControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdminPanel.EntityLayer.Concrete.Other.TestPostgre;
+using AdminPanel.WebAPI.Controllers.Base.Tests;
+using AdminPanle.BusinessLayer.Abstract.Other.TestProduct;
+using AdminPanel.EntityLayer.Concrete.Other.Product;
+
+namespace AdminPanel.WebAPI.Controllers.Settled.TestProduct.Tests
+{
+    [TestClass()]
+    public class ProductControllerTests: BaseControllerTests<ProductController, IBusProduct, Product>
+    {
+
+        [TestInitialize]
+        public void ClassKurulum()
+        {
+            this.Kur();
+        }
+    }
+}
